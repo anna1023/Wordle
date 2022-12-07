206ea4df4963e95ca414caed8723c085d3543016
test <annac101@nycstudents.net>
2022-12-07 00:26:33 -0500

    Final?

---
+++ src/Game.java
@@ -0,0 +1,158 @@
+/**
+ * This is a class that is a game where you guess the word.
+ */
+public class Game {
+    /** This changes the color of the print to green. */
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    /** This changes the color of the print to yellow. */
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    /** This changes the color of the print to red. */
+    public static final String RED = "\033[0;31m";// RED
+    /** This changes the color of the print to white. */
+    public static final String WHITE = "\033[0;37m";   // WHITE
+
+    /** This is the number of rounds happening in the game. */
+    private int rounds;
+    /** This is the answer/word that the player is trying to guess. */
+    private String word;
+    /** This is all the results of the guesses that are printed so far.  */
+    private String results;
+    /** This is the user input */
+    private String guess;
+
+    /**
+     * it sets the custom word to the variable word and set round to 0
+     * @param word - custom word
+     */
+    public Game (String word){
+        rounds = 0;
+        this.word = word;
+        this.results = "";
+    }
+
+    /**
+     * it's a default constructor that generates the word based on a random number generated */
+    public Game() {
+        this.results = "";
+        int num = (int) (Math.random() * 10);
+        if (num == 0) {
+            word = "lemon";
+
+        }
+        if (num ==1) {
+            word = "phone";
+
+        }
+        if (num ==2){
+            word ="mouse";
+
+        }
+        if (num==3){
+            word = "lunch";
+
+        }
+        if (num==4){
+            word = "space";
+
+        }
+        if (num==5){
+            word = "extra";
+
+        }
+        if (num==6){
+            word ="melon";
+
+        }
+        if (num==7){
+            word ="snack";
+
+        }
+        if (num==8){
+            word ="crime";
+
+        }
+        if (num==9){
+            word ="dimly";
+
+        }
+        if (num==10){
+            word ="water";
+
+        }
+        else {
+
+        }
+
+    }
+
+    /** shows all the results of the guesses
+     @return String - results */
+    public String toString(){
+
+        return results;
+    }
+
+    /** gets the guess(user input)
+     @return String - guess */
+    public String getGuess(){
+        return guess;
+    }
+
+    /** sets guess to something
+     * @param newGuess - the thing guess is being set to
+     */
+    public void setGuess(String newGuess){
+        this.guess = newGuess;
+    }
+
+    /** checks the word letter by letter and adds one to round everytime it's run
+     @return String - the user input, color coded to show what's right and what's wrong
+     */
+    public String wordCheck (){
+        String result = "";
+        for (int count = 0;count<5;count++){//checking each letter
+            String letter = guess.substring(count,count+1);
+            if ((letter).equals(word.substring(count,count+1))){ //if the letter is in the right place
+                result += GREEN+letter;
+            }
+            else if (word.indexOf(letter)!=-1){ //check if the letter is not in the right place
+                result += YELLOW+letter;
+            }
+            else {
+                result +=WHITE+letter; //no exist
+            }
+        }
+        rounds ++;
+        results += result + "\n"; //delete a println somewhere
+        return result;
+    }
+
+    /** checks to see if any of the end conditions have been met
+     @return boolean - whether the game is ended or not
+     */
+    public boolean gameEnder(){
+        if (rounds==5 || word.equals(guess)){ //end conditions
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /** announces to the user whether they win or not
+     */
+    public void announcement (){
+        if (gameEnder()){
+            if (word.equals(guess)) {
+                System.out.println("You win! It took you " + rounds + " rounds to win!");
+            }
+            else {
+                System.out.println(RED+"You lose.The word is "+word+".");
+            }
+        }
+
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        Game game1;
+        System.out.println("Welcome to knock off wordle.To create your own word, type in create otherwise type anything else.");
+        String yn = scan.nextLine();
+        if (yn.equals("create")) {
+            System.out.println("Enter your word.");
+            String word = scan.nextLine();
+            int length = word.length();
+            while (length != 5) {
+                System.out.println("Your word is " + length + " not 5 letters long.");
+                System.out.println("Enter your word.");
+                word = scan.nextLine();
+                length = word.length();
+            }
+            for (int count = 0; count <5; count++){
+                System.out.print("\n");
+            }
+            game1 = new Game(word);
+        } else {
+            game1 = new Game();
+        }
+        for (int count = 0; count <6; count++){
+            System.out.println('\n');
+        }
+        System.out.println("Your 5 guess starts now.");
+        for (int round = 0; round<5; round++) {
+            game1.setGuess(scan.nextLine());
+            String guess1 = game1.getGuess();
+            while (guess1.length()!=5){
+                System.out.println("Please enter a 5 letter word");
+                game1.setGuess(scan.nextLine());
+                guess1 = game1.getGuess();
+            }
+            game1.wordCheck();
+            for (int count = 0; count < 11; count++){
+                System.out.println("");
+            }
+            System.out.print(game1);
+            game1.announcement();
+            if (game1.gameEnder()){
+                break;
+            }
+        }
+
     }
 }
+
+
